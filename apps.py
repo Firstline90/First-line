55161053cd5cff532c77b1fcfff360385b9c1f0d
Firstline90 <walidmetwalli94@gmail.com>
2025-11-11 13:53:02 +0300

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(user):
+    st.title("التطبيقات")
+    st.write("هنا تظهر بيانات التطبيقات المرتبطة بالمناديب، مثل Jahez وChefz")
+    st.info("يمكنك تصفية الأداء حسب التطبيق أو عرض تقارير مقارنة")
